ac78b221eaadbd20b0a18eee7a6b8eaa07bfddf2
alesanram <alesanzram@gmail.com>
2022-03-01 14:48:05 +0100

    añadir dist y terminar carrusel
    falta las fotos responsives

---
+++ dist/index.bda4afae.js
@@ -0,0 +1,32 @@
+const texto = document.getElementsByClassName("section__texto");
+const imagen = document.getElementsByClassName("section__imagen");
+const boton = document.getElementsByClassName("section__boton");
+//Js para funcion Tab
+debugger;
+for(let i = 0; i < boton.length; i++)boton[i].addEventListener('click', ()=>{
+    if (imagen[i].classList.contains('active')) imagen[i].classList.remove('active');
+    else imagen[i].classList.add('active');
+});
+debugger;
+const punto = document.getElementsByClassName("tablero__puntos");
+const personajes = document.getElementsByClassName("personajes__carrusel");
+const mapa = document.getElementsByClassName("mapa__carrusel");
+for(let i1 = 0; i1 < punto.length; i1++)punto[i1].children[0].addEventListener('click', ()=>{
+    let operacion = null;
+    if (i1 == 0 || i1 == 3) operacion = "case1";
+    else if (i1 == 1 || i1 == 4) operacion = "case2";
+    else operacion = "case3";
+    if (punto[i1].parentNode.classList.contains("personajes__tablero")) {
+        personajes[0].classList.remove("case1");
+        personajes[0].classList.remove("case2");
+        personajes[0].classList.remove("case3");
+        personajes[0].classList.add(operacion);
+    } else {
+        mapa[0].classList.remove("case1");
+        mapa[0].classList.remove("case2");
+        mapa[0].classList.remove("case3");
+        mapa[0].classList.add(operacion);
+    }
+});
+
+//# sourceMappingURL=index.bda4afae.js.map
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,8 @@
 const texto=document.getElementsByClassName("section__texto");
 const imagen=document.getElementsByClassName("section__imagen");
 const boton=document.getElementsByClassName("section__boton");
-
+//Js para funcion Tab
+debugger
 for(let i=0;i<boton.length;i++) {
     boton[i].addEventListener('click',()=>{
         if(imagen[i].classList.contains('active')){
@@ -12,4 +13,33 @@ for(let i=0;i<boton.length;i++) {
         }
     })
 };
-
+debugger
+const punto=document.getElementsByClassName("tablero__puntos");
+const personajes=document.getElementsByClassName("personajes__carrusel");
+const mapa=document.getElementsByClassName("mapa__carrusel");
+for(let i=0; i<punto.length;i++){
+    punto[i].children[0].addEventListener('click',()=>{
+        let operacion=null;
+        if(i==0 || i==3){
+            operacion="case1";
+        }
+        else if(i==1 || i==4){
+            operacion="case2";
+        }
+        else{
+            operacion="case3";
+        }
+        if(punto[i].parentNode.classList.contains("personajes__tablero")){
+            personajes[0].classList.remove("case1");
+            personajes[0].classList.remove("case2");
+            personajes[0].classList.remove("case3");
+            personajes[0].classList.add(operacion);
+        }
+        else{
+            mapa[0].classList.remove("case1");
+            mapa[0].classList.remove("case2");
+            mapa[0].classList.remove("case3");
+            mapa[0].classList.add(operacion);
+        }
+    })
+};
